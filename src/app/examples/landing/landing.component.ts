12d6ba35b7abd4bba20224c2258db086d64f5972
siddharthadhanetwal <15021992Sd>
2020-05-16 22:06:14 +0530

    adding validation

--- src/app/examples/landing/landing.component.ts
+++ src/app/examples/landing/landing.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl} from '@angular/forms';
-import {NgbdModalContent, NgbdModalComponent} from '../../components/modal/modal.component';
-import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {NgbdModalComponent} from '../../components/modal/modal.component';
 
 @Component({
     selector: 'app-landing',
@@ -12,18 +11,42 @@ import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 export class LandingComponent extends NgbdModalComponent implements OnInit {
   focus: any;
   focus1: any;
-    name = new FormControl('');
-    email = new FormControl('');
-    message = new FormControl('');
 
-  ngOnInit() {}
+    hero = { name: '', email: '', message: '' };
+    private heroForm: FormGroup;
 
+  ngOnInit() {
+      this.heroForm = new FormGroup({
+          'name': new FormControl(this.hero.name, [
+              Validators.required
+          ]),
+          'email': new FormControl(this.hero.email, [
+              Validators.required,
+              Validators.email
+          ]),
+          'message': new FormControl(this.hero.message, [
+              Validators.required,
+              Validators.minLength(10)
+          ])
+      });
+  }
+
+    get name() { return this.heroForm.get('name'); }
+
+    get email() { return this.heroForm.get('email'); }
+
+    get message() { return this.heroForm.get('message'); }
     sendMessage() {
         this.resetFields();
     }
     resetFields() {
-        this.name.setValue('');
-        this.email.setValue('');
-        this.message.setValue('');
+      this.name.reset();
+      this.email.reset();
+      this.message.reset();
+    }
+    checkErrors() {
+      if(this.name.errors || this.email.errors || this.message.errors) {
+          return true;
+      }
     }
 }
